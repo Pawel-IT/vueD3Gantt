d9e76e6b98e32bab8e2ddba8177f4e322d3b601d
PawelK <pawelmesa@gmail.com>
2025-03-27 15:04:00 -0600

    Pretty changes. And added sliders for effect

--- src/components/Basic.vue
+++ src/components/Basic.vue
@@ -1,7 +1,49 @@
 <script setup lang="ts">
 import * as d3 from 'd3'
 import { onMounted, ref, watch } from 'vue'
+import { useIntervalFn } from '@vueuse/core'
 import { color, type Path, range, svg } from 'd3'
+
+const amplitude = ref(1)
+const wavey = ref(0.5)
+
+const visData = (selection, data) => {
+  const lineGenerator = d3
+    .line()
+    .x((d) => d.x)
+    .y((d) => d.y * amplitude.value)
+
+  selection
+    .selectAll('path')
+    .data([null])
+    .join('path')
+    .attr('d', lineGenerator(data))
+    .attr('fill', 'none')
+    .attr('stroke', 'grey')
+    .attr('stroke-width', 2)
+
+  selection
+    .selectAll('circle')
+    .data(data)
+    .join('circle')
+    .attr('r', (d) => d.r + d.y / 3)
+    .attr('cx', (d) => d.x)
+    .attr('cy', (d) => d.y * amplitude.value)
+
+  selection
+    .selectAll('text')
+    .data(data)
+    .join('text')
+    .text((d) => Math.round(d.y * amplitude.value))
+    .attr('x', (d) => d.x)
+    .attr('y', (d) => d.y * amplitude.value)
+    .style('text-anchor', 'middle')
+    .attr('class', 'text_label')
+}
+
+const pause_snake = ref(() => {})
+const resume_snake = ref(() => {})
+const isActive_snake = ref(true)
 
 onMounted(() => {
   const width = 600
@@ -20,30 +62,7 @@ onMounted(() => {
   let t2 = 0
   let grower = 1
 
-  const visData = (selection, data) => {
-    const lineGenerator = d3
-      .line()
-      .x((d) => d.x)
-      .y((d) => d.y)
-
-    selection
-      .selectAll('path')
-      .data([null])
-      .join('path')
-      .attr('d', lineGenerator(data))
-      .attr('fill', 'none')
-      .attr('stroke', 'white')
-
-    selection
-      .selectAll('circle')
-      .data(data)
-      .join('circle')
-      .attr('r', (d) => d.r)
-      .attr('cx', (d) => d.x)
-      .attr('cy', (d) => d.y)
-  }
-
-  setInterval(() => {
+  const { pause, resume, isActive } = useIntervalFn(() => {
     const points = 20
     if (t2 > 10 && t2 < 20) {
       t2++
@@ -58,44 +77,72 @@ onMounted(() => {
     t2 += 1
     const data = d3.range(points).map((d, i) => ({
       x: d * 60 + 50,
-      y: 250 + Math.sin(d * 0.5 + t) * 120,
-      r: grower * 2 * i,
+      y: Math.round(150 + Math.sin(d * wavey.value + t) * 120),
+      r: grower * 3 * Math.cos(i),
     }))
-    //Define
-    // const circles = svg.selectAll('circle').data(data)
+
     svg.call(visData, data)
 
-    //Enter at Start
-    // this works with the merge method below
-    //const circlesEnter = circles.enter().append('circle').attr('r', 20)
-
-    //Update
-    // Merge makes it so it first does the enter state on first render then does updates
-    // circles
-    //   .merge(circlesEnter)
-    //   .attr('cx', (d) => d.x)
-    //   .attr('cy', (d) => d.y)
-
-    // circles
-    //   .join('circle')
-    //   .attr('r', (d, i) => i * grower)
-    //   .attr('cx', (d) => d.x)
-    //   .attr('cy', (d) => d.y)
-
     t += 0.3
+    pause_snake.value = pause
+    resume_snake.value = resume
+    isActive_snake.value = isActive
   }, 100)
 })
 </script>
 
 <template>
-  <svg id="charter"></svg>
+  <svg id="charter">
+    <defs>
+      <linearGradient id="header-shape-gradient" x2="0.35" y2="1">
+        <stop offset="0%" stop-color="var(--color-stop)" />
+        <stop offset="50%" stop-color="var(--color-stop)" />
+        <stop offset="100%" stop-color="var(--color-bot)" />
+      </linearGradient>
+    </defs>
+  </svg>
+
+  <button
+    v-if="isActive_snake.value"
+    @click="pause_snake"
+    style="background-color: darkred; color: white"
+  >
+    Pause
+  </button>
+  <button v-else @click="resume_snake" style="background-color: black; color: white">Resume</button>
+  <label for="amplitude">Amplitude: {{ amplitude }}</label>
+  <input type="range" v-model="amplitude" min="0" max="1.5" step=".05" />
+  <label for="wavey">Wavey: {{ wavey }}</label>
+  <input type="range" v-model="wavey" min="0" max="1.5" step=".05" />
 </template>
 
 <style>
 svg circle {
+  fill: url(#header-shape-gradient) chartreuse;
+  fill-opacity: 0.4;
+  stroke: rgba(0, 0, 0, 0.5);
+  stroke-width: 4;
+  filter: drop-shadow(7px 7px 4px rgba(222, 222, 2, 0.2));
+}
+svg circle:hover {
   fill: chartreuse;
-  opacity: 0.4;
-  stroke: #f2f2f2;
-  stroke-width: 2;
+  fill-opacity: 0.7;
+  stroke: rgba(0, 0, 0, 0.5);
+  stroke-width: 4;
+  filter: drop-shadow(7px 7px 4px rgba(222, 222, 2, 0.2));
+}
+#header-shape-gradient {
+  --color-stop: chartreuse;
+  --color-bot: #faed34;
+}
+
+.text_label {
+  color: white;
+  fill: white;
+  stroke-width: 0.5;
+  stroke: black;
+  font-size: 16px;
+  font-weight: bold;
+  filter: drop-shadow(3px 3px 4px rgba(0, 0, 2, 0.7));
 }
 </style>
